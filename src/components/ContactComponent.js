7720b07b4fb3fe75d8f184a6bf1f691ebacb6e88
Sean Leonard <seanleonardsap@gmail.com>
2021-02-13 12:39:25 +0000

    controlled form corrections

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -175,12 +175,8 @@ class Contact extends Component {
                         name="firstname"
                         placeholder="First Name"
                         value={this.state.firstname}
-                        valid={errors.firstname === ""}
-                        invalid={errors.firstname !== ""}
-                        onBlur={this.handleBlur("firstname")}
                         onChange={this.handleInputChange}
                       />
-                      <FormFeedback>{errors.firstname}</FormFeedback>
                     </Col>
                   </FormGroup>
                   <FormGroup row>
@@ -194,12 +190,8 @@ class Contact extends Component {
                         name="lastname"
                         placeholder="Last Name"
                         value={this.state.lastname}
-                        valid={errors.lastname === ""}
-                        invalid={errors.lastname !== ""}
-                        onBlur={this.handleBlur("lastname")}
                         onChange={this.handleInputChange}
                       />
-                      <FormFeedback>{errors.lastname}</FormFeedback>
                     </Col>
                   </FormGroup>
                   <FormGroup row>
@@ -211,14 +203,10 @@ class Contact extends Component {
                         type="tel"
                         id="telnum"
                         name="telnum"
-                        placeholder="Tel. Number"
+                        placeholder="Tel. number"
                         value={this.state.telnum}
-                        valid={errors.telnum === ""}
-                        invalid={errors.telnum !== ""}
-                        onBlur={this.handleBlur("telnum")}
                         onChange={this.handleInputChange}
                       />
-                      <FormFeedback>{errors.telnum}</FormFeedback>
                     </Col>
                   </FormGroup>
                   <FormGroup row>
@@ -232,12 +220,56 @@ class Contact extends Component {
                         name="email"
                         placeholder="Email"
                         value={this.state.email}
-                        valid={errors.email === ""}
-                        invalid={errors.email !== ""}
-                        onBlur={this.handleBlur("email")}
                         onChange={this.handleInputChange}
                       />
-                      <FormFeedback>{errors.email}</FormFeedback>
+                    </Col>
+                  </FormGroup>
+                  <FormGroup row>
+                    <Col md={{ size: 6, offset: 2 }}>
+                      <FormGroup check>
+                        <Label check>
+                          <Input
+                            type="checkbox"
+                            name="agree"
+                            checked={this.state.agree}
+                            onChange={this.handleInputChange}
+                          />{" "}
+                          <strong>May we contact you?</strong>
+                        </Label>
+                      </FormGroup>
+                    </Col>
+                    <Col md={{ size: 3, offset: 1 }}>
+                      <Input
+                        type="select"
+                        name="contactType"
+                        value={this.state.contactType}
+                        onChange={this.handleInputChange}
+                      >
+                        <option>Tel.</option>
+                        <option>Email</option>
+                      </Input>
+                    </Col>
+                  </FormGroup>
+                  <FormGroup row>
+                    <Label htmlFor="message" md={2}>
+                      Your Feedback
+                    </Label>
+                    <Col md={10}>
+                      <Input
+                        type="textarea"
+                        id="message"
+                        name="message"
+                        rows="12"
+                        value={this.state.message}
+                        onChange={this.handleInputChange}
+                      ></Input>
+                    </Col>
+                  </FormGroup>
+                  <FormGroup row>
+                    <Col md={{ size: 10, offset: 2 }}>
+                      <Button type="submit" color="primary">
+                        Send Feedback
+                      </Button>
                     </Col>
                   </FormGroup>
                 </Form>
