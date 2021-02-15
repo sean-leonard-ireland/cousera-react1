6e986dd43d27061c70687c8dc3a9047297fb6636
Sean Leonard <seanleonardsap@gmail.com>
2021-02-15 18:40:05 +0000

    React Redux Forms Revisited

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -8,7 +8,7 @@ import {
   Label,
 } from "reactstrap";
 import { Link } from "react-router-dom";
-import { Control, LocalForm, Errors } from "react-redux-form";
+import { Control, Form, Errors, actions } from "react-redux-form";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -21,6 +21,7 @@ class Contact extends Component {
   handleSubmit(values) {
     console.log("Current State is: " + JSON.stringify(values));
     alert("Current State is: " + JSON.stringify(values));
+    this.props.resetFeedbackForm();
     //event.preventDefault();
   }
 
@@ -56,12 +57,6 @@ class Contact extends Component {
     this.setState({
       [name]: value,
     });
-  }
-
-  handleSubmit(event) {
-    console.log("Current State is: " + JSON.stringify(this.state));
-    alert("Current State is: " + JSON.stringify(this.state));
-    //event.preventDefault();
   }
 
   handleBlur = (field) => (evt) => {
@@ -173,7 +168,10 @@ class Contact extends Component {
                 <h3>Send us your Feedback</h3>
               </div>
               <div className="col-12 col-md-9">
-                <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                <Form
+                  model="feedback"
+                  onSubmit={(values) => this.handleSubmit(values)}
+                >
                   <Row className="form-group">
                     <Label htmlFor="firstname" md={2}>
                       First Name
@@ -297,7 +295,7 @@ class Contact extends Component {
                       </Button>
                     </Col>
                   </Row>
-                </LocalForm>
+                </Form>
               </div>
             </div>
           </div>
